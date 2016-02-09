e79cec224bfecbdc5080d775d6ebbb647cbc4523
dubrovin <dubrovin@SLAC.STANFORD.EDU>
2016-02-09 17:24:20 +0000

    add src/CalibParsBaseAndor3dV1.py - which was forgotten at last commit

---
+++ src/CalibParsBaseAndor3dV1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#------------------------------
+""":py:class:`CalibParsBaseAndor3dV1` - holds basic calibration metadata parameters for associated detector.
+
+@see :py:class:`PSCalib.CalibPars`, :py:class:`PSCalib.CalibParsStore`.
+
+This software was developed for the SIT project.
+If you use all or part of it, please give an appropriate acknowledgment.
+
+Revision: $Revision$
+
+@version $Id$
+
+@author Mikhail S. Dubrovin
+"""
+#------------------------------
+
+class CalibParsBaseAndor3dV1 :
+
+    ndim = 3 
+    segs = 2
+    rows = 2048
+    cols = 2048
+    size = segs*rows*cols
+    shape = (segs, rows, cols)
+    size_cm = 16 
+    shape_cm = (size_cm,)
+    cmod = (2,10,10,cols,0,0,0,0,0,0,0,0,0,0,0,0)
+        
+    def __init__(self) : pass
+
+#------------------------------
+
