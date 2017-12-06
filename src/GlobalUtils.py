c8bb8fc1d67eb6af8e99ee83cf7854911d55031a
msdubrovin <dubrovin@slac.stanford.edu>
2017-12-06 15:29:02 -0800

    src/GlobalUtils.py - add support for epix10ka

--- src/GlobalUtils.py
+++ src/GlobalUtils.py
@@ -152,6 +152,7 @@ ANDOR3D     = 25
 JUNGFRAU    = 26
 ZYLA        = 27
 EPICSCAM    = 28
+EPIX10KA    = 29
 
 #XAMPS    # N/A data
 #FEXAMP   # N/A data
@@ -164,7 +165,7 @@ EPICSCAM    = 28
 list_of_det_type = (UNDEFINED, CSPAD, CSPAD2X2, PRINCETON, PNCCD, TM6740, \
                     OPAL1000, OPAL2000, OPAL4000, OPAL8000, \
                     ORCAFL40, EPIX, EPIX10K, EPIX100A, FCCD960, ANDOR, ACQIRIS, IMP, QUARTZ4A150, RAYONIX,
-                    EVR, FCCD, TIMEPIX, FLI, PIMAX, ANDOR3D, JUNGFRAU, ZYLA, EPICSCAM)
+                    EVR, FCCD, TIMEPIX, FLI, PIMAX, ANDOR3D, JUNGFRAU, ZYLA, EPICSCAM, EPIX10KA)
 """ List of enumetated detector types"""
 
 list_of_det_names = ('UNDEFINED', 'Cspad', 'Cspad2x2', 'Princeton', 'pnCCD', 'Tm6740', \
@@ -201,7 +202,8 @@ list_of_calib_groups = ('UNDEFINED',
                         'Andor3d::CalibV1',
                         'Jungfrau::CalibV1',
                         'Camera::CalibV1',
-                        'Camera::CalibV1'
+                        'Camera::CalibV1',
+                        'Epix10ka::CalibV1',
                         )
 """ List of enumetated detector calibration groups"""
 
@@ -311,6 +313,7 @@ def det_type_from_source(source) :
     elif ':Jungfrau.'       in str_src : return JUNGFRAU
     elif ':Zyla.'           in str_src : return ZYLA
     elif ':ControlsCamera.' in str_src : return EPICSCAM
+    elif ':Epix10ka.'       in str_src : return EPIX10KA
     else                               : return UNDEFINED
 
 #------------------------------
