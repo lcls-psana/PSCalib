02e711b8cc8b0b2b78a2dac6816d58703f8c3e1e
msdubrovin <msdubrovin@gmail.com>
2022-02-24 15:31:21 -0800

    code cleaning

--- src/CalibParsBaseEpix100aV1.py
+++ src/CalibParsBaseEpix100aV1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#------------------------------
 """
 :py:class:`CalibParsBaseEpix100aV1` - holds basic calibration metadata parameters for associated detector
 =========================================================================================================
@@ -8,7 +7,7 @@ See:
   -  :py:class:`GenericCalibPars`
   -  :py:class:`GlobalUtils`
   -  :py:class:`CalibPars`
-  -  :py:class:`CalibParsStore` 
+  -  :py:class:`CalibParsStore`
   -  :py:class:`CalibParsBaseAndorV1`
   -  :py:class:`CalibParsBaseAndor3dV1`
   -  :py:class:`CalibParsBaseCameraV1`
@@ -25,21 +24,20 @@ If you use all or part of it, please give an appropriate acknowledgment.
 
 Author: Mikhail Dubrovin
 """
-#------------------------------
 
 class CalibParsBaseEpix100aV1(object) :
 
-    ndim = 2 
-    rows = 704 
-    cols = 768 
+    ndim = 2
+    rows = 704
+    cols = 768
     size = rows*cols
     shape = (rows, cols)
-    size_cm = 16 
+    size_cm = 16
     shape_cm = (size_cm,)
     cmod = (4,6,30,30, 0,0,0,0, 0,0,0,0, 0,0,0,0)
     # 4-Epix100a, 1-median for 16 352x96 banks, 20-maximal allowed correction
-         
+
     def __init__(self) : pass
 
-#------------------------------
+# EOF
 
