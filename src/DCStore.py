5a1f659058e5f9b1b5f56ad17e11af242afeeeb3
msdubrovin <msdubrovin@gmail.com>
2022-04-25 11:36:47 -0700

    code cleaning

--- src/DCStore.py
+++ src/DCStore.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#------------------------------
+
 """
 Class :py:class:`DCStore` for the Detector Calibration (DC) project
 ===================================================================
@@ -57,45 +57,36 @@ If you use all or part of it, please give an appropriate acknowledgment.
 Created: 2016 by Mikhail Dubrovin
 """
 from __future__ import print_function
-#------------------------------
 
 import os
 import sys
 from time import time
 
-#import math
-#import numpy as np
-#from PSCalib.DCConfigParameters import cp
 from PSCalib.DCInterface import DCStoreI
 from PSCalib.DCType import DCType
 from PSCalib.DCLogger import log
 from PSCalib.DCUtils import gu, sp, save_object_as_dset, evt_time, delete_object
 
-#------------------------------
 
-def print_warning(obj, metframe) :
+def print_warning(obj, metframe):
     wng = 'INFO: %s.%s - abstract interface method needs to be re-implemented in derived class.' \
           % (obj.__class__.__name__, metframe.f_code.co_name)
     log.warning(wng, obj.__class__.__name__)
     #print wng
     #raise NotImplementedError(wng)
 
-#------------------------------
-#------------------------------
 
-class DCStore(DCStoreI) :
-    
+class DCStore(DCStoreI):
+
     """Class for the Detector Calibration (DC) project
 
     Parameters
-    
+
     path : str - path to the hdf5 file with calibration info
     cmt  : str - comment
     """
 
-#------------------------------
-
-    def __init__(self, path, cmt=None) :
+    def __init__(self, path, cmt=None):
         DCStoreI.__init__(self, path, cmt)
         self._name = self.__class__.__name__
         self._set_file_name(path)
@@ -104,19 +95,17 @@ class DCStore(DCStoreI) :
         self._successor = None
         self._dicctypes = {}
         log.debug('In c-tor for path: %s' % path, self._name)
-        
-#------------------------------
 
-    def _set_file_name(self, path) :
+
+    def _set_file_name(self, path):
         self._fpath = path if isinstance(path, str) else None
-        if self._fpath is None : return
+        if self._fpath is None: return
         root, ext = os.path.splitext(path)
         detname = os.path.basename(root)
         dettype, detid = detname.split('-',1)
         self.set_dettype(dettype)
         self.set_detid(detid)
 
-#------------------------------
 
     def tscfile(self)               : return self._tscfile
 
@@ -125,8 +114,8 @@ class DCStore(DCStoreI) :
     def detid(self)                 : return self._detid
 
     def detname(self)               :
-        if self._dettype is None : return None
-        if self._detid is None : return None
+        if self._dettype is None: return None
+        if self._detid is None: return None
         return '%s-%s' % (self._dettype, self._detid)
 
     def predecessor(self)           : return self._predecessor
@@ -143,53 +132,53 @@ class DCStore(DCStoreI) :
 
     def set_detid(self, detid)      : self._detid = str(detid)
 
-    def set_detname(self, detname) :
-        if not isinstance(detname, str) :
+    def set_detname(self, detname):
+        if not isinstance(detname, str):
             self._dettype, self._detid = None, None
             return
 
         fields = detname.split('-',1)
         self._dettype, self._detid = fields[0], fields[1]
 
-    def set_predecessor(self, pred=None) : self._predecessor = pred 
+    def set_predecessor(self, pred=None): self._predecessor = pred
 
-    def set_successor(self, succ=None)   : self._successor = succ
+    def set_successor(self, succ=None)  : self._successor = succ
 
 
-    def add_ctype(self, ctype, cmt=False) :
-        if not (ctype in gu.calib_names) : 
+    def add_ctype(self, ctype, cmt=False):
+        if not (ctype in gu.calib_names):
             msg = 'ctype "%s" is not in the list of known types:\n  %s' % (ctype, gu.calib_names)
             log.error(msg, self.__class__.__name__)
             return None
-            
-        if ctype in list(self._dicctypes.keys()) :
+
+        if ctype in list(self._dicctypes.keys()):
             return self._dicctypes[ctype]
         o = self._dicctypes[ctype] = DCType(ctype)
 
-        rec = self.make_record('add ctype', ctype, cmt) 
-        if cmt is not False : self.add_history_record(rec)
+        rec = self.make_record('add ctype', ctype, cmt)
+        if cmt is not False: self.add_history_record(rec)
         log.info(rec, self.__class__.__name__)
         return o
 
 
-    def mark_ctype(self, ctype, cmt=False) :
+    def mark_ctype(self, ctype, cmt=False):
         """Marks child object for deletion in save()"""
-        if ctype in list(self._dicctypes.keys()) :
+        if ctype in list(self._dicctypes.keys()):
             self._lst_del_keys.append(ctype)
 
-            rec = self.make_record('del ctype', ctype, cmt) 
-            if cmt is not False : self.add_history_record(rec)
+            rec = self.make_record('del ctype', ctype, cmt)
+            if cmt is not False: self.add_history_record(rec)
             log.info(rec, self.__class__.__name__)
             return ctype
-        else :
+        else:
             msg = 'Marking of non-existent ctype "%s"' % str(ctype)
             log.warning(msg, self._name)
             return None
 
 
-    def mark_ctypes(self) :
+    def mark_ctypes(self):
         """Marks all child objects for deletion in save()"""
-        for ctype in self._dicctypes.keys() :
+        for ctype in self._dicctypes.keys():
             self.mark_ctype(ctype)
             #self._lst_del_keys.append(ctype)
 
@@ -198,23 +187,23 @@ class DCStore(DCStoreI) :
         del self._dicctypes
         #self._dicctypes = {}
         #for ctype in self._dicctypes.keys():
-        #    del self._dicctypes[ctype] 
+        #    del self._dicctypes[ctype]
 
 
-    def clear_ctypes(self) :
-        self._dicctypes.clear()     
+    def clear_ctypes(self):
+        self._dicctypes.clear()
 
 
-    def save(self, path=None, mode='r+') :
-        if path is not None : self._fpath = path
-        if not isinstance(self._fpath, str) :
+    def save(self, path=None, mode='r+'):
+        if path is not None: self._fpath = path
+        if not isinstance(self._fpath, str):
             msg = 'Invalid file name: %s' % str(self._fpath)
             log.error(msg, self.__class__.__name__)
             raise ValueError(msg)
 
         mode_rw = mode if os.path.exists(self._fpath) else 'w'
-        
-        with sp.File(self._fpath, mode_rw) as grp :
+
+        with sp.File(self._fpath, mode_rw) as grp:
 
             msg = '= save(), group %s object for %s' % (grp.name, self.detname())
             log.debug(msg, self._name)
@@ -223,17 +212,17 @@ class DCStore(DCStoreI) :
             ds2 = save_object_as_dset(grp, 'detname',     data=self.detname())     # 'str'
             ds3 = save_object_as_dset(grp, 'detid',       data=self.detid())       # 'str'
             ds4 = save_object_as_dset(grp, 'tscfile',     data=self.tscfile())     # 'double'
-            ds5 = save_object_as_dset(grp, 'predecessor', data=self.predecessor()) # 'str'       
+            ds5 = save_object_as_dset(grp, 'predecessor', data=self.predecessor()) # 'str'
             ds6 = save_object_as_dset(grp, 'successor',   data=self.successor())   # 'str'
 
             # save/delete objects in/from hdf5 file
-            for k,v in self._dicctypes.items() :
-                if k in self._lst_del_keys : delete_object(grp, k)
-                else : v.save(grp)
+            for k,v in self._dicctypes.items():
+                if k in self._lst_del_keys: delete_object(grp, k)
+                else: v.save(grp)
                        #self._dicctypes[k].save(grp)
 
             # deletes items from dictionary
-            for k in self._lst_del_keys :
+            for k in self._lst_del_keys:
                 del self._dicctypes[k]
 
             self._lst_del_keys = []
@@ -244,19 +233,19 @@ class DCStore(DCStoreI) :
             log.info('File %s is updated/saved' % self._fpath, self._name)
 
 
-    def load(self, path=None) : 
+    def load(self, path=None):
 
-        with sp.File(self._fpath, 'r') as grp :
-            
+        with sp.File(self._fpath, 'r') as grp:
+
             #msg = 'Load data from file %s and fill %s object for group "%s"' % (self._fpath, self._name, grp.name)
             #log.info(msg, self._name)
             log.info('Load data from file %s' % self._fpath, self._name)
 
-            for k,v in dict(grp).items() :
+            for k,v in dict(grp).items():
                 #subgrp = v
                 #print '    ', k # , "   ", subg.name #, val, subg.len(), type(subg),
 
-                if isinstance(v, sp.dataset_t) :                    
+                if isinstance(v, sp.dataset_t):
                     log.debug('load dataset "%s"' % k, self._name)
                     if   k == 'dettype'     : self.set_dettype(v[0])
                     elif k == 'detid'       : self.set_detid(v[0])
@@ -264,16 +253,16 @@ class DCStore(DCStoreI) :
                     elif k == 'tscfile'     : self.set_tscfile(v[0])
                     elif k == 'predecessor' : self.set_predecessor(v[0])
                     elif k == 'successor'   : self.set_successor(v[0])
-                    else : log.warning('hdf file has unrecognized dataset "%s"' % k, self._name)
+                    else: log.warning('hdf file has unrecognized dataset "%s"' % k, self._name)
 
-                elif isinstance(v, sp.group_t) :
-                    if self.is_base_group(k,v) : continue
-                    log.debug('load group "%s"' % k, self._name)                    
+                elif isinstance(v, sp.group_t):
+                    if self.is_base_group(k,v): continue
+                    log.debug('load group "%s"' % k, self._name)
                     o = self.add_ctype(k, cmt=False)
                     o.load(v)
- 
 
-    def print_obj(self) :
+
+    def print_obj(self):
         offset = 1 * self._offspace
         self.print_base(offset)
         tsec = self.tscfile()
@@ -289,18 +278,14 @@ class DCStore(DCStoreI) :
         print('%s N types     %s' % (offset, len(self.ctypes())))
         print('%s types       %s' % (offset, str(list(self.ctypes().keys()))))
 
-        for k,v in self.ctypes().items() :
+        for k,v in self.ctypes().items():
         #    #msg='Add type %s as object %s' % (k, v.ctype())
         #    #log.info(msg, self._name)
             v.print_obj()
 
-#------------------------------
-#------------------------------
 #----------- TEST -------------
-#------------------------------
-#------------------------------
 
-def test_DCStore() :
+def test_DCStore():
 
     o = DCStore('cspad-654321.h5')
 
@@ -326,9 +311,8 @@ def test_DCStore() :
     #o.save(None)
     o.load(None)
 
-#------------------------------
 
-def test_DCStore_save() :
+def test_DCStore_save():
 
     import numpy as np
 
@@ -387,48 +371,44 @@ def test_DCStore_save() :
     o.print_obj()
     o.save()
 
-#------------------------------
 
-def test_DCStore_load() :
-
-    import numpy as np
-
-    o = DCStore('cspad-654321.h5')
-    o.load()
-
-    print(50*'_','\ntest o.print()') 
-    o.print_obj()
-
-#------------------------------
-
-def test_DCStore_load_and_save() :
+def test_DCStore_load():
 
     import numpy as np
 
     o = DCStore('cspad-654321.h5')
     o.load()
 
-    print(50*'_','\ntest o.print()') 
+    print(50*'_','\ntest o.print()')
     o.print_obj()
 
-    print(50*'_','\n test o.save(fname)') 
+
+def test_DCStore_load_and_save():
+
+    import numpy as np
+
+    o = DCStore('cspad-654321.h5')
+    o.load()
+
+    print(50*'_','\ntest o.print()')
+    o.print_obj()
+
+    print(50*'_','\n test o.save(fname)')
     o.save('cspad-re-loaded.h5')
 
-#------------------------------
 
-def test() :
-    log.setPrintBits(0o377) 
-    if len(sys.argv)==1    : print('For test(s) use command: python %s <test-number=1-4>' % sys.argv[0])
-    elif(sys.argv[1]=='1') : test_DCStore()        
-    elif(sys.argv[1]=='2') : test_DCStore_save()        
-    elif(sys.argv[1]=='3') : test_DCStore_load()        
-    elif(sys.argv[1]=='4') : test_DCStore_load_and_save()        
-    else : print('Non-expected arguments: sys.argv = %s use 1,2,...' % sys.argv)
+def test():
+    log.setPrintBits(0o377)
+    if len(sys.argv)==1   : print('For test(s) use command: python %s <test-number=1-4>' % sys.argv[0])
+    elif(sys.argv[1]=='1'): test_DCStore()
+    elif(sys.argv[1]=='2'): test_DCStore_save()
+    elif(sys.argv[1]=='3'): test_DCStore_load()
+    elif(sys.argv[1]=='4'): test_DCStore_load_and_save()
+    else: print('Non-expected arguments: sys.argv = %s use 1,2,...' % sys.argv)
 
-#------------------------------
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     test()
     sys.exit( 'End of %s test.' % sys.argv[0])
 
-#------------------------------
+# EOF
